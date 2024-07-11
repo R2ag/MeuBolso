1bed5a510743742db4501d84e56cfceef1cc12e8
Rafael Lacerda <rlag@outlook.com>
2024-07-10 21:35:25 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/rlag/meubolso/MeubolsoApplication.java
@@ -0,0 +1,13 @@
+package br.com.rlag.meubolso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeubolsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeubolsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rlag/meubolso/MeubolsoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rlag.meubolso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeubolsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
